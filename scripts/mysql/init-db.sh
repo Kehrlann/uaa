9a9bc58072c778810c558b7854cb8db1bc0ac418
Daniel Garnier-Moiroux <daniel.garnier-moiroux@broadcom.com>
2024-12-04 17:02:07 +0100

    tests: tooling to run mysql locally

---
+++ scripts/mysql/init-db.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Number of gradle workers times 4, which was somewhat arbitrary but is sufficient in practice.
+# We make extra dbs because a gradle worker ID can exceed the max number of workers.
+NUM_OF_DATABASES_TO_CREATE=24
+
+function initDB() {
+  mysql -uroot -pchangeme <<-EOSQL
+  SET GLOBAL max_connections = 250;
+  CREATE DATABASE uaa DEFAULT CHARACTER SET utf8mb4;
+EOSQL
+}
+
+function createDB() {
+  DATABASE_NAME="uaa_${1}"
+  mysql  -uroot -pchangeme <<-EOSQL
+    CREATE DATABASE $DATABASE_NAME DEFAULT CHARACTER SET utf8mb4;
+EOSQL
+}
+
+initDB
+
+for db_id in `seq 1 $NUM_OF_DATABASES_TO_CREATE`; do
+  createDB $db_id
+done
