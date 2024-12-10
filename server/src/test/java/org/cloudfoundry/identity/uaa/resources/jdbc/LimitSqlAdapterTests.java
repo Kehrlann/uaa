cc7c835f6e35c0280d5876e9bf784d07c738de31
Filip Hanik <fhanik@vmware.com>
2024-12-09 17:17:46 -0800

    WIP - fix unit tests to support all three databases
    search for `@Disabled` to find tests commented out that don't work for MySQL

--- server/src/test/java/org/cloudfoundry/identity/uaa/resources/jdbc/LimitSqlAdapterTests.java
+++ server/src/test/java/org/cloudfoundry/identity/uaa/resources/jdbc/LimitSqlAdapterTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
